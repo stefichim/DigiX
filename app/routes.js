00e7652dac90a18116d008deb0966865202c4f9b
Tudor Timcu <timcuandreitudor@gmail.com>
2015-04-20 12:45:05 +0300

    Added views(Norbert), modifed routes;

--- app/routes.js
+++ app/routes.js
@@ -1,98 +1,31 @@
 // app/routes.js
 module.exports = function (app, passport) {
-
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
     app.get('/', function (req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.render('index.ejs');
     });
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
-    app.get('/login', function (req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('login.ejs', {message: req.flash('loginMessage')});
-    });
-
-    // process the login    form
-    // app.post('/login', do all our passport stuff here);
-
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
-    app.get('/signup', function (req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', {message: req.flash('signupMessage')});
-    });
-
-    // process the signup form
-    app.post('/signup', passport.authenticate('local-signup', {
+    app.post('/login', passport.authenticate('login', {
         successRedirect: '/profile', // redirect to the secure profile section
-        failureRedirect: '/signup', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }));
-
-    app.get('/first', function (req, res, next) {
-
-        if (req.flash('signupFailure').length > 0)
-            res.render('first-page.ejs', {message: req.flash('signupFailure')});
-        else
-            next();
-
-    }, function (req, res) {
-
-        res.render('first-page.ejs', {message: req.flash('loginFailure')});
-
-    });
-
-    app.post('/sign_up', passport.authenticate('signup', {
-        successRedirect: '/first', // redirect to the secure profile section
         failureRedirect: '/', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
 
-    app.post('/log_in', passport.authenticate('login', {
-        successRedirect: '/add_account', // redirect to the secure profile section
-        failureRedirect: '/first', // redirect back to the signup page if there is an error
+    app.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/profile', // redirect to the secure profile section
+        failureRedirect: '/', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
 
-
-    app.get('/add_account', function (req, res) {
-        res.render('add-account-page.ejs');
-    });
-
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function (req, res) {
         res.render('profile.ejs', {
             user: req.user // get the user out of session and pass to template
         });
     });
 
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
     app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
-
-    // process the login form
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/profile', // redirect to the secure profile section
-        failureRedirect: '/login', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }));
 };
 
 // route middleware to make sure a user is logged in
