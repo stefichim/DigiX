25408c74eccbade42fc51d801ab3b642b47422b7
Tudor Timcu <timcuandreitudor@gmail.com>
2015-04-20 14:36:54 +0300

    Fixed db issues; login/signup works;

--- app/routes.js
+++ app/routes.js
@@ -1,40 +1,48 @@
 // app/routes.js
-module.exports = function (app, passport) {
-    app.get('/', function (req, res) {
+module.exports = function(app, passport) {
+    app.get('/', function(req, res) {
         res.render('index.ejs');
     });
 
-    app.post('/login', passport.authenticate('login', {
-        successRedirect: '/profile', // redirect to the secure profile section
-        failureRedirect: '/', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }));
-
     app.post('/signup', passport.authenticate('signup', {
-        successRedirect: '/profile', // redirect to the secure profile section
-        failureRedirect: '/', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
+        successRedirect : '/profile',
+        failureRedirect : '/',
+        failureFlash : true
     }));
 
-    app.get('/profile', isLoggedIn, function (req, res) {
-        res.render('profile.ejs', {
-            user: req.user // get the user out of session and pass to template
+    app.post('/login', passport.authenticate('login', {
+        successRedirect : '/profile',
+        failureRedirect : '/',
+        failureFlash : true
+    }));
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile', {
+            user : req.user
         });
     });
 
-    app.get('/logout', function (req, res) {
+    app.get('/edit_profile', isLoggedIn, function(req, res) {
+        res.render('edit_profile', {
+            user : req.user
+        });
+    });
+
+    app.get('/add_account', isLoggedIn, function(req, res) {
+        res.render('add_account', {
+            user : req.user
+        });
+    });
+
+    app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
 };
 
-// route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
 
-    // if they aren't redirect them to the home page
     res.redirect('/');
 }
