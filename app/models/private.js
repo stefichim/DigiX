f559d8e49480eb6848e17be587c810874299ab9f
Pava <a.pavaloi@gmail.com>
2015-05-26 15:53:49 +0300

    Am uitat de fisierul asta :D.

---
+++ app/models/private.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Pava on 5/25/2015.
+ */
+module.exports={
+    flickr : {
+        consumer_key : '3df130b07fe77f78ba318d87fe192c00',
+        consumer_secret: '1d3e72bacff0745c'
+    },
+    photo : {
+        url: String,
+        tags: [{tag: String}]
+    }
+};
